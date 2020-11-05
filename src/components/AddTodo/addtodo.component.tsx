0ee28665c67606fffc081b1edfa6ed72d24636d3
Vladimir Troubleton <vladimirtroubleton@hotmail.com>
2020-11-05 16:05:02 +0500

    fix form handlers

--- src/components/AddTodo/addtodo.component.tsx
+++ src/components/AddTodo/addtodo.component.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ChangeEventHandler } from "react";
+import React, { ChangeEventHandler, FormEventHandler } from "react";
 import { observer, inject } from "mobx-react";
 
 import "./addtodo.styles.scss";
@@ -8,19 +8,25 @@ import { IAddTodoProps } from "./addtodo.interface";
 // import { notEmpty } from "helpers/validation/rules";
 
 import { Input } from "common/components/Input";
-// import { Button } from "common/components/Button";
+import { Button } from "common/components/Button";
 
 const AddTodo: React.FC<IAddTodoProps> = inject("AppStore")(
   observer(({ todo, onAdd, onChange }) => {
-    const handleAddChange: ChangeEventHandler<HTMLInputElement> = (
-      e: ChangeEvent<HTMLInputElement>
-    ) => {
+    const { name } = todo;
+
+    const handleAddChange: ChangeEventHandler<HTMLInputElement> = (e) => {
       onChange(e.target.value);
     };
 
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+      e.preventDefault();
+      onAdd();
+    };
+
     return (
-      <form>
-        <Input value={todo.name} onChange={handleAddChange} autoFocus />
+      <form onSubmit={handleSubmit}>
+        <Input value={name} onChange={handleAddChange} autoFocus />
+        <Button type="submit">Добавить</Button>
       </form>
     );
   })
